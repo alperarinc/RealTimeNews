891ecee8d74137a25d40afc2a87533758b55886b
alperarinc <alperarinc@hotmail.com>
2023-03-22 23:53:28 +0300

    demo2

--- src/main/java/com/example/realtimenews/NewsController.java
+++ src/main/java/com/example/realtimenews/NewsController.java
@@ -1,18 +1,16 @@
 package com.example.realtimenews;
 
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import netscape.javascript.JSObject;
+
 import org.json.JSONObject;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-import java.awt.*;
-import java.io.Console;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @RestController
@@ -20,38 +18,50 @@ public class NewsController {
 
 
 
-    public List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
-
-    // method for client subscription
+    //public List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
+    public Map<String, SseEmitter> emitters = new HashMap<>();
+    // SUBSCRIPTION
 
     @CrossOrigin
     @RequestMapping(value = "/subscribe" , consumes = MediaType.ALL_VALUE)
-    public SseEmitter subscribe()  {
+    public SseEmitter subscribe(@RequestParam String userID)  {
         SseEmitter sseEmitter = new SseEmitter(Long.MAX_VALUE);
+        sendInitEvent(sseEmitter);
+        emitters.put(userID, sseEmitter);
+
+        sseEmitter.onCompletion(() ->emitters.remove(sseEmitter));
+        sseEmitter.onTimeout(() ->emitters.remove(sseEmitter));
+        sseEmitter.onError((e) ->emitters.remove(sseEmitter));
+
+
+        return sseEmitter;
+    }
+    
+    private void sendInitEvent (SseEmitter sseEmitter) {
+        
         try {
             sseEmitter.send(SseEmitter.event().name("INIT"));
         } catch (IOException e){
             e.printStackTrace();
         }
-        sseEmitter.onCompletion(() ->emitters.remove(sseEmitter));
-        emitters.add(sseEmitter);
-        return sseEmitter;
+        
     }
 
 
-    // method for dispatching events to all clients
-    @PostMapping(value = "/dispatchEvent")
-    public void dispatchEventToClient (@RequestParam String title, @RequestParam String text){
+
+    @PostMapping(value = "/dispatchEventToSpecificUser")
+    public void dispatchEventToClient (@RequestParam String title, @RequestParam String text,  @RequestParam String userID){
 
         String eventFormatted = new JSONObject()
                 .put("title", title)
                 .put("text" , text).toString();
 
-        for ( SseEmitter emitter : emitters){
+        SseEmitter sseEmitter = emitters.get(userID);
+        if (sseEmitter != null){
             try {
-                emitter.send(SseEmitter.event().name("latestNews").data(eventFormatted));
-            } catch (IOException e){
-                emitters.remove(emitter);
+                sseEmitter.send(SseEmitter.event().name("latestNews").data(eventFormatted));
+            }catch (IOException e){
+                emitters.remove(sseEmitter);
             }
         }
     }
